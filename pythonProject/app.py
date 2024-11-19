3858e8f7bd37ad063fa580de0dcb3c63f5bbae61
Kayab Khan <khankayab10@gmail.com>
2024-11-19 20:43:38 +0530

    Update app.py

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -10,16 +10,23 @@ st.logo(image=image,size='large')
 st.title('About')
 st.write('So, this is my project on resturant data analysis and plotting them on map')
 st.header('go to recommend section on sidebar for project')
-st.write('you may argue that this is not Machine Learning project and youre right, it is not, it is simple GEOSPATIAL project.' 
-         ' however this project uses many data science techniques, such as feature engineering, data analysis')
-st.write('for this project i used two data set one from a fake shady intern company and one from kaggle of swiggy dataset,'
-         ' but swiggy dataset did not contain cooordinates so i had to use a geooding libraies to gather all the coordaintes from address and city data available'
-         ' that took me almost 4 hours')
-st.write('it is not a extraordinary project, and utilises basic conditional arguments in the backend '
-         ' i will do a recommendation system that utilises content based filtering and collaborative based filtering after my exam,')
+st.write('This is a restaurant recommendation software that ask for location and cuisine and plot them on map;
 
-st.write(' i did this to understand geospatial projects')
-st.write('more to come after exam, abhi chemistry padne ja raha 😎')
+'- extracted dataset from kaggle(from swiggy)'
+'- performed EDA and feature engineering on it to filter necessary information,'
+'- implemented a "geocoding" library API that extracted coordinates of the restaurant provided with address and city details,'
+'- visualized restaurant on map using "folium library",'
+'- created and hosted on streamlit.')
+# st.write('you may argue that this is not Machine Learning project and youre right, it is not, it is simple GEOSPATIAL project.' 
+#          ' however this project uses many data science techniques, such as feature engineering, data analysis')
+# st.write('for this project i used two data set one from a fake shady intern company and one from kaggle of swiggy dataset,'
+#          ' but swiggy dataset did not contain cooordinates so i had to use a geooding libraies to gather all the coordaintes from address and city data available'
+#          ' that took me almost 4 hours')
+# st.write('it is not a extraordinary project, and utilises basic conditional arguments in the backend '
+#          ' i will do a recommendation system that utilises content based filtering and collaborative based filtering after my exam,')
+
+st.write('I always wanted to geospatial projects and this is just a beginning')
+st.write('more to come after exam😎🫂')
 
 
 
